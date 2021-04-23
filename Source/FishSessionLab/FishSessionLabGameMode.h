0daddf158b64da6967a584739d30b9664275818f
JulianFish <zero94516@gmail.com>
2021-04-23 09:26:47 +0900

    first commit(project file)

---
+++ Source/FishSessionLab/FishSessionLabGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FishSessionLabGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFishSessionLabGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFishSessionLabGameMode();
+};
+
+
+
